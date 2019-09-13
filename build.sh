e795fdf8a9cbf26dd28ba53d2e866c56ced318cd
root <aobersnel@apnex.com.au>
2019-09-13 13:39:51 +1000

    Updates..

--- build.sh
+++ build.sh
@@ -6,8 +6,3 @@ IMAGE_NAME="apnex/control-ntp"
 # remove existing and rebuild image
 docker rmi -f ${IMAGE_NAME} 2>/dev/null
 docker build --rm --no-cache -t ${IMAGE_NAME} -f control-ntp.docker .
-
-#CONTAINER_NAME="ntp"
-
-# any additional docker run options you may want.
-#DOCKER_OPTS="--cap-add SYS_NICE --cap-add SYS_RESOURCE --cap-add SYS_TIME"
