3f7a6ef80d46a216ef604eed63562adff75afa3e
root <aobersnel@apnex.com.au>
2019-09-13 13:38:45 +1000

    Updates..

--- stop.sh
+++ stop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-SERVICENAME="control-ntp"
+SERVICENAME="ntp"
 
-printf "[apnex/${SERVICENAME}] stopping\n" 1>&2
+printf "[apnex/control-${SERVICENAME}] stopping\n" 1>&2
 docker rm -f "${SERVICENAME}" 2>/dev/null
 
 # remove dangling image
-docker rm -v $(docker ps -qa -f name="${SERVICENAME}" -f status=exited) 2>/dev/null
+docker rm -v $(docker ps -qa -f name="control-${SERVICENAME}" -f status=exited) 2>/dev/null
